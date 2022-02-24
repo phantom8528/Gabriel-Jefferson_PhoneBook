0b855a234519e0ea64071a89c398bd157be987f3
Gabriel Jefferson <phantom8528@gmail.com>
2022-02-24 12:11:02 -0500

    Ticket 1 - Spacing, Option Verification, SEE NOTES IN COMMENTS

--- GabrielJefferson_PhoneBook.py
+++ GabrielJefferson_PhoneBook.py
@@ -1,6 +1,9 @@
 #Name: Gabriel Jefferson
 #Assignment: Module 2, Day 5, PhoneBook.py
 
+#from operator import is_
+
+
 terminate_prog = False
 instruction_menu = '''\nGabriel's Electronic Phone Book\n
 ==================================\n
@@ -12,12 +15,19 @@ Please Type The Number Corresponding with the following options\n
 5. Quit\n
 ==================================='''
 #________________FUNCTIONS_______________________________
+def verify_option_input(input): #<-- created a function that verifies that the option inputted is the option available
+    #pass
+    is_between = 1 <= input <= 5 #<-- This returns a boolean
+    if is_between == False:
+        print("\nSorry,\n\nThis is not an option") #<-- Catches any "int" that isn't an option
+    
+
 def add_an_entry():
     global entName
-    entName = input("Enter Name: ")
-    entNumb = input("Enter Number: ")
+    entName = input("\nEnter Name: ") #<-- Just added some spacing
+    entNumb = input("\nEnter Number: ")#<-- Just added some spacing
     phone_book_entries[entName] = entNumb
-    print ("Entry Added")
+    print ("\nEntry Added") #<-- Just added some spacing
     
     # SELF HOMEWORK -->iterate through dictionary to confirm, and return a confirmation message
 def list_all_entries():
@@ -37,8 +47,9 @@ phone_book_entries = {} #<---Dictionary for phone book
 while not(terminate_prog):
 
     print (instruction_menu)
-
+    #global user_input #<-- made user_input a global variable so that it could be used by functions
     user_input = int(input("What would you like to do\n\nChoose an option (1-5): "))
+    verify_option_input(user_input)
 
     if user_input == 1: # <-- Looking up and entry
         #pass
